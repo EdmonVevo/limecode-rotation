43bd5da1edd8270c37d02c8ae36b9ae5ceb00620
Edmon <edmonvevo@gmail.com>
2019-09-06 14:37:12 +0400

    Ready

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(()=>{
-	// $(document).on('mouseenter','.image_container',function(){
-	// 	$(this).addClass('open_card');
-	// })
-	// $(document).on('mouseleave','.image_container',function(){
-	// 	$(this).removeClass('open_card');
-	// })
-}) 
